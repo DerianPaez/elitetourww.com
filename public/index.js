d44ebde597ca92a7cc1722d3318c4f113d80ad1e
James Loor <mendozajames02@hotmail.com>
2022-02-21 02:21:34 -0500

    Añadir sección de contacto con estilos, diseño responsivo y lógica

--- public/index.js
+++ public/index.js
@@ -16,3 +16,29 @@ links.forEach(link => {
     menu.classList.toggle('active')
   })
 })
+
+
+const $form = document.getElementById("contact-form-id")
+
+$form.addEventListener("submit", handleSubmit)
+
+async function handleSubmit(event) {
+  event.preventDefault()
+
+  const form = new FormData(this)
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+
+  if(response.ok) {
+    this.reset()
+    alert('¡Gracias por contactarnos!, En breve nos comunicaremos para responder su consulta.')
+  } else {
+    this.reset()
+    alert('¡Hubo un problema al enviar el formulario!')
+  }
+}
