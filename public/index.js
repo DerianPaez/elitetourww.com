fcca62e569122a839d0d022029e4e926a1fc6d76
James Loor <mendozajames02@hotmail.com>
2022-02-23 01:36:17 -0500

    Añadir funcion para averiguar cual navegador se usa & segun el resultado se quita background-attachment

--- public/index.js
+++ public/index.js
@@ -4,6 +4,8 @@ const menu = document.getElementById('menu')
 const menuOpen = document.getElementById('menu-icon-open')
 const menuClose = document.getElementById('menu-icon-close')
 const links = document.querySelectorAll('#menu a')
+const banner = document.getElementById('banner-id')
+
 
 menuOpen.addEventListener('click', () => {
   menu.classList.toggle('active')
@@ -43,4 +45,24 @@ async function handleSubmit(event) {
     this.reset()
     alert('¡Hubo un problema al enviar el formulario!')
   }
+}
+
+let getBrowserInfo = () => {
+  var ua= navigator.userAgent, tem,
+  M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+  if(/trident/i.test(M[1])){
+      tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+      return 'IE '+(tem[1] || '');
+  }
+  if(M[1]=== 'Chrome'){
+      tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+      if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+  }
+  M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+  if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+  return M.join(' ');
+}
+
+if(!getBrowserInfo().toLowerCase().includes('safari')) {
+  banner.style.backgroundAttachment = 'fixed'
 }
